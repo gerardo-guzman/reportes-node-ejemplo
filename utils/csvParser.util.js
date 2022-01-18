1b47bcdde86f7d7bc420173fbb7b4590386c066b
Gerardo Guzmán <gerardo.guzman.desarrollo@gmail.com>
2022-01-18 09:46:41 -0600

    retorna un zip de los archivos

--- utils/csvParser.util.js
+++ utils/csvParser.util.js
@@ -1,20 +1,62 @@
 const { Parser } = require('json2csv');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const zip = new require('node-zip')();
 
-const csvCreator = (jsonResult = {}) => {
-    const csvs = [];
-    try {
-        const fields = ['ID_campaign', 'campaign_name', 'fecha', 'country', 'ID_detalle', 'seller', 'total'];
-        const options = { fields };
-        const parser = new Parser(options);
-        Object.keys(jsonResult).forEach(key => {
-            const csv = parser.parse(jsonResult[key]);
-            //console.log('CSV', csv);
-            csvs.push(csv);
-        });
-    } catch (error) {
-        console.error(error);
+class CsvUtil {
+
+    csvs = [];
+    fileNames = [];
+
+    constructor (jsonResult) {
+        this.jsonResult = jsonResult;
     }
-    return csvs;
+
+    csvParser() {
+        try {
+            const fields = ['ID_campaign', 'campaign_name', 'fecha', 'country', 'ID_detalle', 'seller', 'total'];
+            const options = { fields };
+            const parser = new Parser(options);
+            Object.keys(this.jsonResult).forEach(key => {
+                const csv = parser.parse(this.jsonResult[key]);
+                //console.log('CSV', csv);
+                this.csvs.push(csv);
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    saveFileCopy() {
+        const promises = [];
+        for (const csv in this.csvs) {
+            const name = 'temp/' + uuidv4() + '.csv';
+            const fileProm = this.writePromiseBased(name, csv);
+            promises.push(fileProm);
+            this.fileNames.push(name);
+        }
+        return promises;
+    }
+
+    writePromiseBased(name, data) {
+        return new Promise((resolve, reject) => {
+            fs.write(name, data, (err) => {
+                if (err) reject(err);
+                resolve(true);
+            });
+        })
+    }
+
+    zipFiles() {
+        for (const csv of this.csvs) {
+            const name = uuidv4() + '.csv';
+            zip.file(name, csv)
+        }
+        return zip.generate({base64:false,compression:'DEFLATE'});
+    }
+
 }
 
-module.exports = csvCreator;
+
+
+module.exports = CsvUtil;
